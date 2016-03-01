57e4fe303804243e26acf36cc5d9fe80e211dc23
Jason Data <Winning@JASONs-MacBook-Pro.local>
2016-02-29 18:18:31 -0800

    added index view and capybara index view specs

--- ChargeApp/spec/features/user_sees_lists_spec.rb
+++ ChargeApp/spec/features/user_sees_lists_spec.rb
@@ -1,10 +1,22 @@
 require "spec_helper"
 
 feature "User sees tables" do
-  scenario "Failed, Disputed, and Successful Charges" do
+  before do
     visit '/charges'
-    expect(page).to have_content "Failed Charges"
-    expect(page).to have_content "Disputed Charges"
+  end
+
+  scenario "with correct Successful data" do
     expect(page).to have_content "Successful Charges"
+    expect(page).to have_selector('.successful_charges tbody > tr', count: 10)
+  end
+
+  scenario "with correct Failed data" do
+    expect(page).to have_content "Failed Charges"
+    expect(page).to have_selector('.failed_charges tbody > tr', count: 5)
+  end
+
+  scenario "with correct Disputed data" do
+    expect(page).to have_content "Disputed Charges"
+    expect(page).to have_selector('.failed_charges tbody > tr', count: 5)
   end
 end
